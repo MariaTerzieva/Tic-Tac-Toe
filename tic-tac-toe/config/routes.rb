f7886f33a87ddd38856aa6e8e3ae3872dea616ee
Maria Terzieva <mariya.g.terzieva@gmail.com>
2015-07-29 16:47:20 +0300

    Make the index page the default page

--- tic-tac-toe/config/routes.rb
+++ tic-tac-toe/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root :to => 'tic_tac_toe#dashboard'
+  root :to => 'tic_tac_toe#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
