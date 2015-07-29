fd89f06eddcfa964606a1ba7c66e80a90e94ddce
Maria Terzieva <mariya.g.terzieva@gmail.com>
2015-07-29 14:45:21 +0300

    Make the game page the default page

--- tic-tac-toe/config/routes.rb
+++ tic-tac-toe/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root :to => 'tic_tac_toe#dashboard'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
