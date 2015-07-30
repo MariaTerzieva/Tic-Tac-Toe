d021902e39c0347fee2b11ad93c719459ec4bb08
Maria Terzieva <mariya.g.terzieva@gmail.com>
2015-07-30 22:03:38 +0300

    Add Player MVC

---
+++ tic-tac-toe/app/controllers/players_controller.rb
@@ -0,0 +1,19 @@
+class PlayersController < ApplicationController
+  def save
+    @player = Player.find_by(name: params[:winner])
+
+    if @player
+      @player.update(number_of_wins: @player.number_of_wins.succ)
+    else
+      @player = Player.new(name: params[:winner], number_of_wins: 1)
+      @player.save
+    end
+  end
+
+  def leaderboard
+    @players = Player.all
+  end
+
+  def draw
+  end
+end
